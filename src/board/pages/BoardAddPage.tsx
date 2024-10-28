f2b8ccdae2ff00cd13d976068dc697efe0902513
sungoh <sungoh@gmail.com>
2024-10-28 15:17:56 +0900

    board 복구_SO

---
+++ src/board/pages/BoardAddPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BoardAddComponent from '../component/BoardAddComponent';
+
+function BoardAddPage() {
+    return (
+        <div>
+            <BoardAddComponent/>
+        </div>
+    );
+}
+
+export default BoardAddPage;
